67e1e1354ae78d49e378862e212fd394eb3caaf1
Miguel Mejia <miguelmejia@uni.pe>
2019-07-19 17:57:01 -0500

    yacasix104

--- src/main/java/app/repositorios/RepositorioDeudas.java
+++ src/main/java/app/repositorios/RepositorioDeudas.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import app.modelo.Deudas;
 import app.modelo.Permiso;
 
-public interface RepositorioDeudas extends CrudRepository<Deudas, Integer> {
+public interface RepositorioDeudas extends CrudRepository<Deudas, String> {
 
 }
