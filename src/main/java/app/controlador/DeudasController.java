ef9e68352fa41a84ace77c378ac713d2db364734
Miguel Mejia <miguelmejia@uni.pe>
2019-07-19 17:41:10 -0500

    yacasix101

--- src/main/java/app/controlador/DeudasController.java
+++ src/main/java/app/controlador/DeudasController.java
@@ -1,7 +1,53 @@
 package app.controlador;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import app.modelo.Deudas;
+import app.modelo.Pago;
+import app.repositorios.RepositorioDeudas;
+
+@CrossOrigin
+@RestController
+@RequestMapping(path="/servicios")
 
 public class DeudasController {
+	@Autowired
+	private RepositorioDeudas deudasrepo;
+	/*
+	@CrossOrigin
+	@PostMapping(path="/insertar")
+	public @ResponseBody String agregarDeuda(@RequestParam int iddeuda,
+												@RequestParam String fdni,
+												@RequestParam String fidservicio,
+												@RequestParam String montodeuda) {
+		
+	}
+	*/
 	
+	@CrossOrigin
+    @GetMapping(path="/listar")
+	@ResponseBody
+	public Iterable<Deudas> listarPermisos(@RequestParam String numDni) {
+        Iterable<Deudas> listaDeu = deudasrepo.findAll();
+        ArrayList<Deudas> listDeudas = new ArrayList<Deudas>();
+     
+        for(Deudas p:listaDeu) {
+        	if(p.getFdni()==numDni) {
+        		listDeudas.add(p);
+        	}
+        }
+        return listDeudas;
+    }
+
+			
+			
 }
